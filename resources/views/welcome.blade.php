a2870c0e6c1ae014ac321dd9591066c68c84ab65
Josephat Onkoba <josephatonkoba30@gmail.com>
2024-05-16 10:37:20 +0300

    centered the content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
 
-<div class="flex justify-center items-center h-full">
+<div class="flex place-content-center items-center h-full">
     <div class="max-w-lg p-6 bg-white border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Welcome aboard!</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">As you navigate through your attachment journey, our system will serve as your trusted companion, offering tools to streamline your tasks, resources to enhance your skills, and insights to propel your success.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <a href="#" class="inline-flex items-center px-3 py-2 text-md font-medium text-center text-gray bg-white rounded-md hover:bg-custom-color-2 hover:text-white focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800 border border-gray-500">
             Get started
             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
