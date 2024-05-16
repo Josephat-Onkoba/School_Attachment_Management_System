8d0fed069ab5b183dc33d1b3e847b47cc2b25462
Josephat Onkoba <josephatonkoba30@gmail.com>
2024-05-16 10:37:55 +0300

    new routes added

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('layouts/sidebar');
+Route::get('/HOD/dashboard', function () {
+    return view('HOD/dashboard/dashboard');
+});
+
+Route::get('/auth/login', function () {
+    return view('auth/login');
+});
+
+Route::get('/auth/activate', function () {
+    return view('auth/activate');
+});
+
+Route::get('/auth/set-password', function () {
+    return view('auth/setpassword');
 });
