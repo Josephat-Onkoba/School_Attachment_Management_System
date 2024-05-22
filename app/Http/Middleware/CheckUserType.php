c239a095ec810d2b44a69935924ee37479507990
Josephat Onkoba <josephatonkoba30@gmail.com>
2024-05-22 16:33:55 +0300

    Created check user type middleware

---
+++ app/Http/Middleware/CheckUserType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $type): Response
+    {
+        if (auth()->check() && auth()->user()->user_type == $type) {
+            return $next($request);
+        }
+
+        return redirect('/'); // Redirect to home or another page if unauthorized
+    }
+}
